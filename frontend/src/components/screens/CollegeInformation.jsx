57c8778a07685fff1ed8f61a749dd391231bd4f5
Al Amin <alaminpgmr@gmail.com>
2024-10-31 09:30:23 +0600

    "Updated CollegeInformation component: added new icons, badges, and buttons, and rearranged layout and styling."

--- frontend/src/components/screens/CollegeInformation.jsx
+++ frontend/src/components/screens/CollegeInformation.jsx
@@ -1,4 +1,9 @@
-import { Check } from "lucide-react";
+import { Check, GraduationCap, Info } from "lucide-react";
+import { PiCricket, PiStarFill } from "react-icons/pi";
+import { VscServerProcess } from "react-icons/vsc";
+import { Link } from "react-router-dom";
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
 import EventCard from "./EventCard";
 import ResearchCard from "./ResearchCard";
 import ReviewCard from "./ReviewCard";
@@ -9,7 +14,7 @@ const CheckItem = ({ text }) => (
       className="text-primary_main p-1.5 border-gray-100 rounded-full border"
       size={32}
     />
-    <p className="text-[15px] text-[#777777] leading-relaxed font-roboto">
+    <p className=" text-xs xl:text-[15px] text-[#777777] leading-relaxed font-roboto">
       {text}
     </p>
   </div>
@@ -44,10 +49,10 @@ const CollegeInformation = ({ college }) => {
   ];
 
   return (
-    <div className="py-16 lg:py-30 relative">
+    <div className="py-16 lg:py-30 relative bg-[#FEFEFE]">
       <div className="container_fluid">
-        <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
-          <div className="lg:col-span-4">
+        <div className="flex flex-wrap lg:flex-nowrap gap-8 relative">
+          <div className="max-w-[978px] w-full">
             <div className="space-y-6">
               <h2 className="text-3xl lg:text-5xl font-bold text-secondary_main-950 pb-4">
                 {name}
@@ -121,9 +126,76 @@ const CollegeInformation = ({ college }) => {
               </section>
             </div>
           </div>
-          <aside>
+          <aside className="w-full lg:w-[306px]">
             {/* other site */}
-            Hello world
+            <div className="bg-white shadow rounded-lg sticky top-20">
+              <div className="">
+                <h2 className="text-xl bg-secondary_main p-3 rounded-t-lg font-semibold text-white flex items-center gap-2">
+                  <Info size={20} /> Information
+                </h2>
+                <div>
+                  <p className="text-sm text-[#777777] leading-relaxed font-roboto border-b p-4 flex items-center justify-between">
+                    <span className="inline-flex items-center gap-2 font-bold text-secondary_main">
+                      <GraduationCap
+                        size={20}
+                        className="text-primary_main"
+                      />{" "}
+                      Admission Open:
+                    </span>{" "}
+                    {admissionDate}
+                  </p>
+
+                  <p className="text-sm text-[#777777] leading-relaxed font-roboto p-4 border-b flex justify-between">
+                    <span className="inline-flex items-center gap-2 font-bold text-secondary_main">
+                      <PiCricket
+                        size={20}
+                        className="text-primary_main"
+                      />{" "}
+                      Sports:
+                    </span>{" "}
+                    {sports?.slice(0, 3).map((sport) => (
+                      <Badge
+                        key={sport._id}
+                        className="bg-primary_main text-[10px]"
+                      >
+                        {sport}
+                      </Badge>
+                    ))}
+                  </p>
+
+                  <p className="text-sm text-[#777777] leading-relaxed font-roboto p-4 border-b flex items-center justify-between">
+                    <span className="inline-flex items-center gap-2 font-bold text-secondary_main">
+                      <PiStarFill
+                        size={20}
+                        className="text-primary_main"
+                      />{" "}
+                      Rating:
+                    </span>
+                    <span className="text-white inline-flex gap-3 items-center bg-secondary_main px-2.5 py-[1.5px] rounded">
+                      <PiStarFill /> {rating}
+                    </span>
+                  </p>
+
+                  <p className="text-sm text-[#777777] leading-relaxed font-roboto p-4 border-b">
+                    <VscServerProcess
+                      size={18}
+                      className="text-primary_main inline-block -mt-1"
+                    />{" "}
+                    <b className="text-secondary_main">Admission Process:</b>{" "}
+                    {admissionProcess}
+                  </p>
+
+                  <Link
+                    to="/admission"
+                    className="p-4 block"
+                  >
+                    <Button className="w-full py-5 rounded-lg inline-block">
+                      Admission Open
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </aside>
         </div>
       </div>
