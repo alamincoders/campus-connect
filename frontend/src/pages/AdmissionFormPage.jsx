c605d33a945c59baf27a37675c150feef9a0c023
Al Amin <alaminpgmr@gmail.com>
2024-11-02 08:05:16 +0600

    "Updated .gitignore to remove newline at end of file, and modified AdmissionFormPage.jsx to add js-cookie import, use cookies to get user id, and check if user has already applied to any college"

--- frontend/src/pages/AdmissionFormPage.jsx
+++ frontend/src/pages/AdmissionFormPage.jsx
@@ -2,6 +2,7 @@ import AdmissionForm from "@/components/screens/AdmissionForm";
 import { BreadcrumbSection } from "@/components/shared/Breadcrumb";
 import Spinner from "@/components/ui/spinner";
 import { useGetAdmissionCollegeQuery } from "@/redux/api/collegesApi";
+import Cookies from "js-cookie";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
@@ -9,11 +10,14 @@ const AdmissionFormPage = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   const {
-    data: college,
+    data: colleges,
     error,
     isLoading: collegeLoading,
   } = useGetAdmissionCollegeQuery();
+
+  const user_id = JSON.parse(Cookies.get("user"))?.id;
 
   if (error) {
     console.error(error);
@@ -26,17 +30,21 @@ const AdmissionFormPage = () => {
     return <Spinner />;
   }
 
-  if (college.data.length === 1) {
+  // Check if the user has already applied to any college
+  const hasApplied = colleges?.data.some(
+    (college) => college.userId === user_id
+  );
+
+  if (hasApplied) {
     return (
       <div className="py-16 lg:py-30 bg-[#EFF7FF]">
-        <div className="flex  items-center justify-center p-6">
-          <p className="text-center text-secondary_main ">
-            You have applied already ({college.data.length} college) maximum
-            number of colleges.
+        <div className="flex items-center justify-center p-6">
+          <p className="text-center text-secondary_main">
+            You have already applied to the maximum number of colleges.
           </p>
           <Link
             to="/my-college"
-            className="text-primary_main px-4 py-4 underline underline-offset-4 decoration-primary_main decoration-wavy "
+            className="text-primary_main px-4 py-4 underline underline-offset-4 decoration-primary_main decoration-wavy"
           >
             See Your Colleges
           </Link>
