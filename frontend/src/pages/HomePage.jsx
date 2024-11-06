a904e0ac2479506038ec6012fd792f6054fc12dc
Al Amin <alaminpgmr@gmail.com>
2024-11-06 20:45:11 +0600

    "Added useEffect hook to HomePage component to scroll to top on render"

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,8 +6,12 @@ import NewsLetter from "@/components/screens/NewsLetter";
 import OurColleges from "@/components/screens/OurColleges";
 import ResearchPapersSection from "@/components/screens/ResearchPapersSection";
 import ReviewSection from "@/components/screens/ReviewSection";
+import { useEffect } from "react";
 
 const HomePage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <HeroSlider />
